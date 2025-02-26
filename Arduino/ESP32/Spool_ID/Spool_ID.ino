8bcac3af426cc178ad164dab5d7f4720583b3f8c
DnG-Crafts <140936256+DnG-Crafts@users.noreply.github.com>
2025-02-26 08:29:34 +0800

    littlefs
    migrate to littlefs from spiffs

---
+++ Arduino/ESP32/Spool_ID/Spool_ID.ino
@@ -0,0 +1,382 @@
+#include <SPI.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WebServer.h>
+#include <LittleFS.h>
+#include "src/includes.h"
+
+#define SS_PIN 5
+#define RST_PIN 21
+#define SPK_PIN 27
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;
+MFRC522::MIFARE_Key ekey;
+WebServer webServer(80);
+AES aes;
+
+IPAddress Server_IP(10, 1, 0, 1);
+IPAddress Subnet_Mask(255, 255, 255, 0);
+String spoolData = "AB1240276A210100100000FF016500000100000000000000";
+String AP_SSID = "K2_RFID";
+String AP_PASS = "password";
+String WIFI_SSID = "";
+String WIFI_PASS = "";
+String WIFI_HOSTNAME = "k2.local";
+bool encrypted = false;
+
+void setup()
+{
+  LittleFS.begin(true);
+  loadConfig();
+  SPI.begin();
+  mfrc522.PCD_Init();
+  key = {255, 255, 255, 255, 255, 255};
+  pinMode(SPK_PIN, OUTPUT);
+  if (AP_SSID == "" || AP_PASS == "")
+  {
+    AP_SSID = "K2_RFID";
+    AP_PASS = "password";
+  }
+  WiFi.softAPConfig(Server_IP, Server_IP, Subnet_Mask);
+  WiFi.softAP(AP_SSID.c_str(), AP_PASS.c_str());
+  WiFi.softAPConfig(Server_IP, Server_IP, Subnet_Mask);
+
+  if (WIFI_SSID != "" && WIFI_PASS != "")
+  {
+    WiFi.setAutoReconnect(true);
+    WiFi.hostname(WIFI_HOSTNAME);
+    WiFi.begin(WIFI_SSID.c_str(), WIFI_PASS.c_str());
+    if (WiFi.waitForConnectResult() == WL_CONNECTED)
+    {
+      IPAddress LAN_IP = WiFi.localIP();
+    }
+  }
+  if (WIFI_HOSTNAME != "")
+  {
+    String mdnsHost = WIFI_HOSTNAME;
+    mdnsHost.replace(".local", "");
+    MDNS.begin(mdnsHost.c_str());
+  }
+
+  webServer.on("/config", HTTP_GET, handleConfig);
+  webServer.on("/index.html", HTTP_GET, handleIndex);
+  webServer.on("/", HTTP_GET, handleIndex);
+  webServer.on("/material_database.json", HTTP_GET, handleDb);
+  webServer.on("/config", HTTP_POST, handleConfigP);
+  webServer.on("/spooldata", HTTP_POST, handleSpoolData);
+  webServer.onNotFound(handle404);
+  webServer.begin();
+}
+
+void loop()
+{
+  webServer.handleClient();
+  if (!mfrc522.PICC_IsNewCardPresent())
+    return;
+
+  if (!mfrc522.PICC_ReadCardSerial())
+    return;
+
+  encrypted = false;
+
+  MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI && piccType != MFRC522::PICC_TYPE_MIFARE_1K && piccType != MFRC522::PICC_TYPE_MIFARE_4K)
+  {
+    tone(SPK_PIN, 400, 400);
+    delay(2000);
+    return;
+  }
+
+  createKey();
+
+  MFRC522::StatusCode status;
+  status = (MFRC522::StatusCode)mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 7, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK)
+  {
+    if (!mfrc522.PICC_IsNewCardPresent())
+      return;
+    if (!mfrc522.PICC_ReadCardSerial())
+      return;
+    status = (MFRC522::StatusCode)mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 7, &ekey, &(mfrc522.uid));
+    if (status != MFRC522::STATUS_OK)
+    {
+      tone(SPK_PIN, 400, 150);
+      delay(300);
+      tone(SPK_PIN, 400, 150);
+      delay(2000);
+      return;
+    }
+    encrypted = true;
+  }
+
+  byte blockData[17];
+  byte encData[16];
+  int blockID = 4;
+  for (int i = 0; i < spoolData.length(); i += 16)
+  {
+    spoolData.substring(i, i + 16).getBytes(blockData, 17);
+    if (blockID >= 4 && blockID < 7)
+    {
+      aes.encrypt(1, blockData, encData);
+      mfrc522.MIFARE_Write(blockID, encData, 16);
+    }
+    blockID++;
+  }
+
+  if (!encrypted)
+  {
+    byte buffer[18];
+    byte byteCount = sizeof(buffer);
+    byte block = 7;
+    status = mfrc522.MIFARE_Read(block, buffer, &byteCount);
+    int y = 0;
+    for (int i = 10; i < 16; i++)
+    {
+      buffer[i] = ekey.keyByte[y];
+      y++;
+    }
+    for (int i = 0; i < 6; i++)
+    {
+      buffer[i] = ekey.keyByte[i];
+    }
+    mfrc522.MIFARE_Write(7, buffer, 16);
+  }
+
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
+  tone(SPK_PIN, 1000, 200);
+  delay(2000);
+}
+
+void createKey()
+{
+  int x = 0;
+  byte uid[16];
+  byte bufOut[16];
+  for (int i = 0; i < 16; i++)
+  {
+    if (x >= 4)
+      x = 0;
+    uid[i] = mfrc522.uid.uidByte[x];
+    x++;
+  }
+  aes.encrypt(0, uid, bufOut);
+  for (int i = 0; i < 6; i++)
+  {
+    ekey.keyByte[i] = bufOut[i];
+  }
+}
+
+void handleIndex()
+{
+  webServer.send_P(200, "text/html", indexData, sizeof(indexData) - 1);
+}
+
+void handle404()
+{
+  webServer.send(404, "text/plain", "Not Found");
+}
+
+void handleDb()
+{
+  webServer.sendHeader("Content-Encoding", "gzip");
+  webServer.send_P(200, "application/json", material_database, sizeof(material_database));
+}
+
+void handleConfig()
+{
+  String htmStr = AP_SSID + "|-|" + WIFI_SSID + "|-|" + WIFI_HOSTNAME;
+  webServer.setContentLength(htmStr.length());
+  webServer.send(200, "text/plain", htmStr);
+}
+
+void handleConfigP()
+{
+  if (webServer.hasArg("ap_ssid") && webServer.hasArg("ap_pass") && webServer.hasArg("wifi_ssid") && webServer.hasArg("wifi_pass") && webServer.hasArg("wifi_host"))
+  {
+    AP_SSID = webServer.arg("ap_ssid");
+    if (!webServer.arg("ap_pass").equals("********"))
+    {
+      AP_PASS = webServer.arg("ap_pass");
+    }
+    WIFI_SSID = webServer.arg("wifi_ssid");
+    if (!webServer.arg("wifi_pass").equals("********"))
+    {
+      WIFI_PASS = webServer.arg("wifi_pass");
+    }
+    WIFI_HOSTNAME = webServer.arg("wifi_host");
+    File file = LittleFS.open("/config.ini", "w");
+    if (file)
+    {
+      file.print("\r\nAP_SSID=" + AP_SSID + "\r\nAP_PASS=" + AP_PASS + "\r\nWIFI_SSID=" + WIFI_SSID + "\r\nWIFI_PASS=" + WIFI_PASS + "\r\nWIFI_HOST=" + WIFI_HOSTNAME + "\r\n");
+      file.close();
+    }
+    String htmStr = "OK";
+    webServer.setContentLength(htmStr.length());
+    webServer.send(200, "text/plain", htmStr);
+    delay(1000);
+    ESP.restart();
+  }
+  else
+  {
+    webServer.send(417, "text/plain", "Expectation Failed");
+  }
+}
+
+void handleSpoolData()
+{
+  if (webServer.hasArg("materialColor") && webServer.hasArg("materialType") && webServer.hasArg("materialWeight"))
+  {
+    String materialColor = webServer.arg("materialColor");
+    materialColor.replace("#", "");
+    String filamentId = "1" + webServer.arg("materialType"); // material_database.json
+    String vendorId = "0276"; // 0276 creality
+    String color = "0" + materialColor;
+    String filamentLen = GetMaterialLength(webServer.arg("materialWeight"));
+    String serialNum = String(random(100000, 999999)); // 000001
+    String reserve = "000000";
+    spoolData = "AB124" + vendorId + "A2" + filamentId + color + filamentLen + serialNum + reserve + "00000000";
+    File file = LittleFS.open("/spool.ini", "w");
+    if (file)
+    {
+      file.print(spoolData);
+      file.close();
+    }
+    String htmStr = "OK";
+    webServer.setContentLength(htmStr.length());
+    webServer.send(200, "text/plain", htmStr);
+  }
+  else
+  {
+    webServer.send(417, "text/plain", "Expectation Failed");
+  }
+}
+
+String GetMaterialLength(String materialWeight)
+{
+  if (materialWeight == "1 KG")
+  {
+    return "0330";
+  }
+  else if (materialWeight == "750 G")
+  {
+    return "0247";
+  }
+  else if (materialWeight == "600 G")
+  {
+    return "0198";
+  }
+  else if (materialWeight == "500 G")
+  {
+    return "0165";
+  }
+  else if (materialWeight == "250 G")
+  {
+    return "0082";
+  }
+  return "0330";
+}
+
+void loadConfig()
+{
+  if (LittleFS.exists("/config.ini"))
+  {
+    File file = LittleFS.open("/config.ini", "r");
+    if (file)
+    {
+      String iniData;
+      while (file.available())
+      {
+        char chnk = file.read();
+        iniData += chnk;
+      }
+      file.close();
+      if (instr(iniData, "AP_SSID="))
+      {
+        AP_SSID = split(iniData, "AP_SSID=", "\r\n");
+        AP_SSID.trim();
+      }
+
+      if (instr(iniData, "AP_PASS="))
+      {
+        AP_PASS = split(iniData, "AP_PASS=", "\r\n");
+        AP_PASS.trim();
+      }
+
+      if (instr(iniData, "WIFI_SSID="))
+      {
+        WIFI_SSID = split(iniData, "WIFI_SSID=", "\r\n");
+        WIFI_SSID.trim();
+      }
+
+      if (instr(iniData, "WIFI_PASS="))
+      {
+        WIFI_PASS = split(iniData, "WIFI_PASS=", "\r\n");
+        WIFI_PASS.trim();
+      }
+
+      if (instr(iniData, "WIFI_HOST="))
+      {
+        WIFI_HOSTNAME = split(iniData, "WIFI_HOST=", "\r\n");
+        WIFI_HOSTNAME.trim();
+      }
+    }
+  }
+  else
+  {
+    File file = LittleFS.open("/config.ini", "w");
+    if (file)
+    {
+      file.print("\r\nAP_SSID=" + AP_SSID + "\r\nAP_PASS=" + AP_PASS + "\r\nWIFI_SSID=" + WIFI_SSID + "\r\nWIFI_PASS=" + WIFI_PASS + "\r\nWIFI_HOST=" + WIFI_HOSTNAME + "\r\n");
+      file.close();
+    }
+  }
+
+  if (LittleFS.exists("/spool.ini"))
+  {
+    File file = LittleFS.open("/spool.ini", "r");
+    if (file)
+    {
+      String iniData;
+      while (file.available())
+      {
+        char chnk = file.read();
+        iniData += chnk;
+      }
+      file.close();
+      spoolData = iniData;
+    }
+  }
+  else
+  {
+    File file = LittleFS.open("/spool.ini", "w");
+    if (file)
+    {
+      file.print(spoolData);
+      file.close();
+    }
+  }
+}
+
+String split(String str, String from, String to)
+{
+  String tmpstr = str;
+  tmpstr.toLowerCase();
+  from.toLowerCase();
+  to.toLowerCase();
+  int pos1 = tmpstr.indexOf(from);
+  int pos2 = tmpstr.indexOf(to, pos1 + from.length());
+  String retval = str.substring(pos1 + from.length(), pos2);
+  return retval;
+}
+
+bool instr(String str, String search)
+{
+  int result = str.indexOf(search);
+  if (result == -1)
+  {
+    return false;
+  }
+  return true;
+}
